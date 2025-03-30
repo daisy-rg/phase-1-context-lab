fe993b47af7efe5771f804aa1302d976b85356ff
daisy <chpdaisy@gmail.com>
2025-03-30 22:35:11 +0300

    done

--- index.js
+++ index.js
@@ -72,4 +72,15 @@ const allWagesFor = function () {
 
     return payable
 }
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+      return arrayOfEmployeeRecords.reduce(function(memo, rec){
+          return memo + allWagesFor.call(rec)
+      }, 0)
+  }
 
